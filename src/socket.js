2eefd0917a35e7ec9fb79f9d839ab5d420614d60
Alan Balbo <alan.balbo@gmail.com>
2018-05-05 01:19:38 +0200

    Now uses socket.io

--- src/socket.js
+++ src/socket.js
@@ -2,27 +2,23 @@
 import WebSocket from 'ws';
 import _ from 'lodash';
 import Command from './models/command';
+import io from 'socket.io';
 
 export default {
-
-
   start(server) {
-    this.wss = new WebSocket.Server({ server });
-    this.wss.broadcast = (data) => {
-      this.wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      });
-    };
-    this.wss.on('connection', (ws) => {
-      ws.send('Hi');
-      ws.on('message', (message) => { 
-        Command.findOne({_id: message}).then((cmd) => {
-          if (cmd) {
-            this.wss.broadcast(JSON.stringify(Object.assign(cmd, {time: Date.now()})));
-          }
-        }).catch((err) => console.error(err));
+
+    this.wss = io(server);
+    this.wss.on('connection', (socket) => {
+
+      socket.on('message', (message) => {
+        Command
+          .findOne({ _id: JSON.parse(message) })
+          .then((cmd) => {
+            if (cmd) {
+              this.wss.emit('broadcast', JSON.stringify(Object.assign(cmd, { time: Date.now() })));
+            }
+          })
+          .catch((err) => console.error(err));
       });
     });
   }
